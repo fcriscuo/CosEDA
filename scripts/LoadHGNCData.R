027de9317191433f8ecf6f2f70034b11841d679f
fcriscuo <genomicdatasci@gmail.com>
2021-04-12 14:29:58 -0700

    Load HGNC and Cosmic data; harmonize HGNC gene names

---
+++ scripts/LoadHGNCData.R
@@ -0,0 +1,33 @@
+## ---------------------------
+##
+## Script name:  LoadHGNCData.R
+##
+## Purpose of script: Load the HGNC data from a local file
+##
+## Author:Fred Criscuolo
+##
+## Date Created: 2021-04-08
+##
+## Copyright (c) Fred Criscuolo, 2021
+## Email: genomicdatasci@gmail.com
+##
+## ---------------------------
+##
+## Notes:
+##   
+##
+## ---------------------------
+
+## load up the packages we will need:  (uncomment as required)
+
+require(tidyverse)
+require(data.table)
+# source("functions/packages.R")       # loads up all the packages we need
+
+## ---------------------------
+
+## load up our functions into memory
+
+# source("functions/summarise_data.R") 
+
+## ---------------------------
